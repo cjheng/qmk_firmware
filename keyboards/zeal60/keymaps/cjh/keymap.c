5d40174075f1e943de32a0d270b7cb03c03a20d2
Cheng-Jie Heng <jie.heng@gmail.com>
2019-01-28 23:46:25 -0500

    Fix macOS volume controls
    Change to macOS specific volume controls to allow shift+option 1/4 tick volume increments/decrements.

--- keyboards/zeal60/keymaps/cjh/keymap.c
+++ keyboards/zeal60/keymaps/cjh/keymap.c
@@ -4,6 +4,9 @@
 
 #define MT_BSPC MT(MOD_LCTL, KC_BSPC)
 #define FN4_CAP LT(4, KC_CAPS)
+#define KC_MMTE KC__MUTE
+#define KC_MVLD KC__VOLDOWN
+#define KC_MVLU KC__VOLUP
 
 uint8_t previous_backlight_effect = 255;
 
@@ -66,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [2] = LAYOUT_60_ansi(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     KC_TRNS, KC_MRWD, KC_MPLY, KC_MFFD, KC_P7,   KC_P8,   KC_P9,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_SLCK, KC_PAUS, KC_TRNS,
-    KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_P4,   KC_P5,   KC_P6,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_MMTE, KC_MVLD, KC_MVLU, KC_P4,   KC_P5,   KC_P6,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
